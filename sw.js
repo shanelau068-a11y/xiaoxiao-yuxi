72622a11d56cef55ac85d406da2cc6083f218c1a
Lesch Hildegard <shanelau068@gmail.com>
2026-08-13 14:01:24 +0800

    Add full second grade curriculum map

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-self.addEventListener('install',event=>event.waitUntil(caches.open('yuwen-v1').then(cache=>cache.addAll(['./','./index.html','./styles.css','./app.js','./manifest.webmanifest']))));
+self.addEventListener('install',event=>event.waitUntil(caches.open('yuwen-v2').then(cache=>cache.addAll(['./','./index.html','./styles.css','./app.js','./manifest.webmanifest']))));
 self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request))));
